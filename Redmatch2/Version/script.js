067067e66e60346b8adb4294d32770c5b58d29d6
Red <rugbugredfern@gmail.com>
2021-01-13 18:05:02 -0800

    js doesnt work

--- Redmatch2/Version/script.js
+++ Redmatch2/Version/script.js
@@ -2,4 +2,4 @@ var url = (window.location != window.parent.location)
 	? document.referrer
 	: document.location.href;
 
-document.write('You are currently running the game on ' + url);
+// document.write('You are currently running the game on ' + url);
